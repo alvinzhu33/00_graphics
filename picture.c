f5b8aa7b613635ea7ef3c8fef58c77c29812c10e
Alvin Zhu <alvinzhu33@yahoo.com>
2017-02-03 22:59:28 -0500

    Update on c

--- picture.c
+++ picture.c
@@ -1,16 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
 
 int main(){
   int fd = open("image.ppm", O_CREAT | O_WRONLY | 0644);
-  char * buffer = "P3 500 500 255\n";
+  char buffer[50];
+  sprintf(buffer, "P3 500 500 255\n");
   write(fd, buffer, sizeof(buffer));
-  
+
   int x, y, r, g, b;
   x = 0;
   y = 0;
-  while(y < 10){
-    while(x < 10){
-      buffer = "0 255 255\n";
+  while(y < 500){
+    while(x < 500){
+      sprintf(buffer, "0 255 255\n");
       write(fd, buffer, sizeof(buffer));
 
       x++;
